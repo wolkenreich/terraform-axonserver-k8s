b61a905de6355ebad6767fcda47f23c3b7fe7776
Francesco Latini <francesco.latini@gmail.com>
2024-07-19 16:02:44 +0200

    Use autocluster only when there's a license

--- configmap.tf
+++ configmap.tf
@@ -1,14 +1,14 @@
-data "template_file" "axonserver_properties" {
-  template = file("${path.module}/conf/axonserver.properties.tmpl")
-
-  vars = {
-    first_name      = "${var.cluster_name}-1"
-    public_domain   = var.public_domain
-    namespace       = kubernetes_namespace.as_demo.id
-    internal_token  = random_uuid.internal_token.result
-    devmode_enabled = var.devmode_enabled
-  }
-}
+#data "template_file" "axonserver_properties" {
+#  template = file("${path.module}/conf/axonserver.properties.tmpl")
+#
+#  vars = {
+#    first_name      = "${var.cluster_name}-1"
+#    public_domain   = var.public_domain
+#    namespace       = kubernetes_namespace.as_demo.id
+#    internal_token  = random_uuid.internal_token.result
+#    devmode_enabled = var.devmode_enabled
+#  }
+#}
 
 resource "kubernetes_config_map" "axonserver_properties" {
   metadata {
@@ -17,6 +17,14 @@ resource "kubernetes_config_map" "axonserver_properties" {
   }
 
   data = {
-    "axonserver.properties" = data.template_file.axonserver_properties.rendered
+    "axonserver.properties" = templatefile(
+      "${path.module}/conf/axonserver.properties.tftpl",
+      {
+        first_name      = "${var.cluster_name}-1",
+        public_domain   = var.public_domain,
+        namespace       = kubernetes_namespace.as_demo.id,
+        internal_token  = random_uuid.internal_token.result,
+        devmode_enabled = var.devmode_enabled
+      })
   }
 }
