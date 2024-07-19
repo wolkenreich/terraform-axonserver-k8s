7f7ef9001a572a39f97b90cd491ec730c8211b62
Francesco Latini <francesco.latini@gmail.com>
2024-07-19 17:00:48 +0200

    Fixed the variable name for console auth

--- configmap.tf
+++ configmap.tf
@@ -2,11 +2,12 @@ data "template_file" "axonserver_properties" {
   template = file("${path.module}/conf/axonserver.properties.tftpl")
 
   vars = {
-    first_name      = "${var.cluster_name}-1"
-    public_domain   = var.public_domain
-    namespace       = kubernetes_namespace.as_demo.id
-    internal_token  = random_uuid.internal_token.result
-    devmode_enabled = var.devmode_enabled
+    first_name             = "${var.cluster_name}-1"
+    public_domain          = var.public_domain
+    namespace              = kubernetes_namespace.as_demo.id
+    internal_token         = random_uuid.internal_token.result
+    devmode_enabled        = var.devmode_enabled
+    console_authentication = var.console_authentication
   }
 }
 
