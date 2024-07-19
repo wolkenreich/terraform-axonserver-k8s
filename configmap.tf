f20e85a7c6de5581c469f8ba703379f610403599
Francesco Latini <francesco.latini@gmail.com>
2024-07-19 16:45:34 +0200

    Fixing the variables to render

--- configmap.tf
+++ configmap.tf
@@ -12,11 +12,11 @@
 
 locals {
   properties = templatefile("${path.module}/conf/axonserver.properties.tftpl", {
-    first_name      = "${var.cluster_name}-1"
-    public_domain   = var.public_domain
+    first_name      = "$${var.cluster_name}-1"
+    public_domain   = "$${var.public_domain}"
     namespace       = kubernetes_namespace.as_demo.id
     internal_token  = random_uuid.internal_token.result
-    devmode_enabled = var.devmode_enabled
+    devmode_enabled = "$${var.devmode_enabled}"
   })
 }
 
