e79346b0c556aec4a4a732adee9ee635159a1ca3
Francesco Latini <francesco.latini@gmail.com>
2024-07-19 16:31:26 +0200

    Using a different approach

--- configmap.tf
+++ configmap.tf
@@ -10,6 +10,16 @@
 #  }
 #}
 
+locals {
+  properties = templatefile("${path.module}/conf/axonserver.properties.tftpl", {
+    first_name      = "${var.cluster_name}-1"
+    public_domain   = var.public_domain
+    namespace       = kubernetes_namespace.as_demo.id
+    internal_token  = random_uuid.internal_token.result
+    devmode_enabled = var.devmode_enabled
+  })
+}
+
 resource "kubernetes_config_map" "axonserver_properties" {
   metadata {
     name      = "axonserver.properties"
@@ -17,14 +27,6 @@ resource "kubernetes_config_map" "axonserver_properties" {
   }
 
   data = {
-    "axonserver.properties" = templatefile(
-      "${path.module}/conf/axonserver.properties.tftpl",
-      {
-        "first_name"      = "${var.cluster_name}-1",
-        "public_domain"   = var.public_domain,
-        "namespace"       = kubernetes_namespace.as_demo.id,
-        "internal_token"  = random_uuid.internal_token.result,
-        "devmode_enabled" = var.devmode_enabled
-      })
+    "axonserver.properties" = local.properties
   }
 }
