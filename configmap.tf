8b7ef4a2b8300c24f066caf08433625c29740471
Francesco Latini <francesco.latini@axoniq.io>
2026-05-27 21:37:07 +0200

    Update configmap.tf

--- configmap.tf
+++ configmap.tf
@@ -1,12 +1,13 @@
 locals {
   axonserver_properties = var.axonserver_properties == "" ? templatefile("${path.module}/conf/axonserver.properties.tftpl", {
-    first_name             = "${var.cluster_name}-1"
-    public_domain          = var.public_domain
-    namespace              = var.create_namespace ? kubernetes_namespace.axonserver[0].id : data.kubernetes_namespace.axonserver[0].id
-    internal_token         = random_uuid.internal_token.result
-    devmode_enabled        = var.devmode_enabled
-    console_authentication = var.console_authentication
-    accesscontrol_enabled  = var.accesscontrol_enabled
+    first_name              = "${var.cluster_name}-1"
+    public_domain           = var.public_domain
+    namespace               = var.create_namespace ? kubernetes_namespace.axonserver[0].id : data.kubernetes_namespace.axonserver[0].id
+    internal_token          = random_uuid.internal_token.result
+    devmode_enabled         = var.devmode_enabled
+    platform_authentication = var.platform_authentication
+    admin_password          = var.admin_password
+    accesscontrol_enabled   = var.accesscontrol_enabled
   }) : var.axonserver_properties
 }
 
